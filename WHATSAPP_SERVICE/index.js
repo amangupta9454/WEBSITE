9db7cfa468564ec19ad2a703d2deda26c1465bc6
AMAN GUPTA <ag0567688@gmail.com>
2026-07-06 15:24:29 +0530

    Clean up for pure Docker deployment

--- WHATSAPP_SERVICE/index.js
+++ WHATSAPP_SERVICE/index.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-// Set Puppeteer Cache Dir so Render Node.js environment can find the installed Chromium
-process.env.PUPPETEER_CACHE_DIR = process.env.PUPPETEER_CACHE_DIR || '/opt/render/project/puppeteer';
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
