7d8bb94639dae1a9a5d895df419d42a491a2b61e
AMAN GUPTA <ag0567688@gmail.com>
2026-07-06 14:54:41 +0530

    Fix local environment issues for WHATSAPP_SERVICE

--- WHATSAPP_SERVICE/index.js
+++ WHATSAPP_SERVICE/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
-// Set Puppeteer Cache Dir so Render Node.js environment can find the installed Chromium
-process.env.PUPPETEER_CACHE_DIR = process.env.PUPPETEER_CACHE_DIR || '/opt/render/project/puppeteer';
+// Set Puppeteer Cache Dir only if running on Render
+if (process.env.RENDER) {
+  process.env.PUPPETEER_CACHE_DIR = '/opt/render/project/puppeteer';
+}
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
